7bab75378900fe0ab01eb5d1733397c20b2f9641
Fabian Kanczurski <fabian.kanczurski@gmail.com>
2025-10-13 18:32:27 +0200

    colorsheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import "@mantine/core/styles.css";
-
 import {
   ColorSchemeScript,
   MantineProvider,
   mantineHtmlProps,
 } from "@mantine/core";
-
 import ClientLayout from "@/components/appShell/ClientLayout";
 
 export const metadata = {
@@ -19,12 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html
+      lang="en"
+      {...mantineHtmlProps}
+      data-mantine-color-scheme="dark" // ← dodaj ręcznie
+    >
       <head>
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body>
-        <MantineProvider>
+        <MantineProvider defaultColorScheme="dark" forceColorScheme="dark">
           <ClientLayout>{children}</ClientLayout>
         </MantineProvider>
       </body>
