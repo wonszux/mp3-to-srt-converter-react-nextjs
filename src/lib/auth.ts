8a847088780029c804f960e467fd79348d681e8c
Fabian Kanczurski <fabian.kanczurski@gmail.com>
2025-10-24 13:18:34 +0200

    bla bla

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  plugins: [nextCookies()], // musi być ostatni
 });
---
+++ src/server/users.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+
+export const signIn = async (
+  email: string,
+  password: string,
+  rememberMe: boolean = true
+) => {
+  await auth.api.signInEmail({
+    body: { email, password, rememberMe },
+  });
+};
+
+export const signUp = async (email: string, password: string, name: string) => {
+  await auth.api.signUpEmail({
+    body: { email, password, name },
+  });
+};
+
+export const setPassword = async (
+  newPassword: string,
+  headers: HeadersInit
+) => {
+  await auth.api.setPassword({
+    body: { newPassword },
+    headers,
+  });
+};
