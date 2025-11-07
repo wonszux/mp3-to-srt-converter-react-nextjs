57dc9aa36b8dffba4ee2cd4b64d485730cfd075a
wonszux <fabian.kanczurski@gmail.com>
2025-11-07 16:12:08 +0100

    asdasdasd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@mantine/core/styles.css";
+import "@mantine/dropzone/styles.css";
 import {
   ColorSchemeScript,
   MantineProvider,
--- src/components/fileInput/fileinput.tsx
+++ src/components/fileInput/fileinput.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { Button, Container, Stack, Text, TextInput } from "@mantine/core";
-import { Dropzone, DropzoneProps } from "@mantine/dropzone";
+import {
+  Button,
+  Container,
+  Stack,
+  Text,
+  TextInput,
+  Group,
+} from "@mantine/core";
+import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import { IconFileDownload, IconLink } from "@tabler/icons-react";
+import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
 import { useState } from "react";
 
 export default function FileInput() {
@@ -41,27 +49,75 @@ export default function FileInput() {
             Wklej link
           </Button>
         </Container>
-        <Container
-          w="100%"
-          mih={300}
-          bdrs={35}
-          bg="#181818ff"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "dashed 1px gray",
-          }}
-        >
-          {active === "file" ? (
-            <Stack align="center" justify="center">
-              <IconFileDownload size={50} />
-              <Text>Kliknij lub przeciągnij plik, aby go przesłać</Text>
-              <Button variant="outline" color="gray" radius={6}>
-                Wybierz plik
-              </Button>
-            </Stack>
-          ) : (
+        {active === "file" ? (
+          <Dropzone
+            onDrop={(files) => console.log("accepted files", files)}
+            onReject={(files) => console.log("rejected files", files)}
+            maxSize={5 * 1024 ** 2}
+            accept={IMAGE_MIME_TYPE}
+            //bg = asdasd
+            w="100%"
+            mih={300}
+            radius={35}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "dashed 1px gray",
+            }}
+          >
+            <Group
+              justify="center"
+              gap="xl"
+              mih={220}
+              style={{ pointerEvents: "none" }}
+            >
+              <Dropzone.Accept>
+                <IconUpload
+                  size={52}
+                  color="var(--mantine-color-blue-6)"
+                  stroke={1.5}
+                />
+              </Dropzone.Accept>
+              <Dropzone.Reject>
+                <IconX
+                  size={52}
+                  color="var(--mantine-color-red-6)"
+                  stroke={1.5}
+                />
+              </Dropzone.Reject>
+              <Dropzone.Idle>
+                <IconPhoto
+                  size={52}
+                  color="var(--mantine-color-dimmed)"
+                  stroke={1.5}
+                />
+              </Dropzone.Idle>
+
+              <div>
+                <Text size="xl" inline>
+                  Drag images here or click to select files
+                </Text>
+                <Text size="sm" c="dimmed" inline mt={7}>
+                  Attach as many files as you like, each file should not exceed
+                  5mb
+                </Text>
+              </div>
+            </Group>
+          </Dropzone>
+        ) : (
+          <Container
+            w="100%"
+            mih={300}
+            bdrs={35}
+            bg="#181818ff"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "dashed 1px gray",
+            }}
+          >
             <Stack align="center" justify="center" w="100%" p="md">
               <IconLink size={50} />
               <Text>Wklej link, a następnie kliknij "Generuj"</Text>
@@ -75,8 +131,8 @@ export default function FileInput() {
                 Generuj
               </Button>
             </Stack>
-          )}
-        </Container>
+          </Container>
+        )}
       </Stack>
     </Container>
   );
