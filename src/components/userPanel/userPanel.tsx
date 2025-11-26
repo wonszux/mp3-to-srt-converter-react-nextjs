79369eb87504521407f73fc018ecebccecd39314
Fabian Kanczurski <fabian.kanczurski@gmail.com>
2025-11-26 14:42:03 +0100

    aaa

--- src/components/userPanel/userPanel.tsx
+++ src/components/userPanel/userPanel.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Tabs, Container } from '@mantine/core'
-import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react'
+import { IconBubbleText, IconBook, IconSettings } from '@tabler/icons-react'
 
 import UserHistory from '@/components/userPanel/userHistory'
 import FileInput from '../fileInput/fileinput'
@@ -9,10 +9,10 @@ export default function userPanel() {
   return (
     <Tabs variant="outline" radius="md" p="md" defaultValue="transcription">
       <Tabs.List>
-        <Tabs.Tab value="transcription" leftSection={<IconPhoto size={12} />}>
+        <Tabs.Tab value="transcription" leftSection={<IconBubbleText size={12} />}>
           Transkrypcja
         </Tabs.Tab>
-        <Tabs.Tab value="history" leftSection={<IconMessageCircle size={12} />}>
+        <Tabs.Tab value="history" leftSection={<IconBook size={12} />}>
           Historia
         </Tabs.Tab>
         <Tabs.Tab value="settings" leftSection={<IconSettings size={12} />}>
